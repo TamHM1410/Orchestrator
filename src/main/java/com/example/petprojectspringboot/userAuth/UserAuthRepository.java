391d1fd2421d6ad440dcf3ed5ee23916f369595f
Huynh Minh Tam <hunhminhtam@gmail.com>
2025-12-22 22:12:55 +0700

    auth

---
+++ src/main/java/com/example/petprojectspringboot/userAuth/UserAuthRepository.java
@@ -0,0 +1,9 @@
+package com.example.petprojectspringboot.userAuth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAuthRepository extends JpaRepository<UserAuth,Long> , JpaSpecificationExecutor<UserAuth> {
+}
---
+++ src/main/java/com/example/petprojectspringboot/userAuth/UserAuthService.java
@@ -0,0 +1,12 @@
+package com.example.petprojectspringboot.userAuth;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserAuthService {
+    private UserAuthRepository userAuthRepository;
+
+
+}
